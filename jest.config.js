7739c87e8b8ee326490101ac518bf99790f7a82a
benaor <benjamingirard25@gmail.com>
2022-02-24 15:59:28 +0100

    Add react-testing-library and jsdom and setup it for the testing app

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  verbose: true,
   collectCoverageFrom: [
     '{components,pages}/**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
@@ -27,5 +28,6 @@ module.exports = {
     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   },
-  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$']
-};
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+  testEnvironment: 'jest-environment-jsdom'
+}

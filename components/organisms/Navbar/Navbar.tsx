a8afbf6644a0c1b4d62c0b1685f1f3031d76d192
benaor <benjamingirard25@gmail.com>
2022-03-01 23:13:18 +0100

    Edit navbar

--- components/organisms/Navbar/Navbar.tsx
+++ components/organisms/Navbar/Navbar.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react'
 import Logo from '../../atoms/Logo'
+import Navigation from '../../molecules/Navigation'
+import { ListNavItemProp } from '../../molecules/Navigation/Navigation.types'
 
 const Navbar = () => {
   const styles: React.CSSProperties = {
     display: 'flex',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    minHeight: '100px',
+    borderBottom: '1px solid #E5E5E5'
   }
+
+  const listNavItem: Array<ListNavItemProp> = [
+    {
+      name: 'Accueil',
+      href: '/Home'
+    },
+    {
+      name: 'Blog',
+      href: '/Blog'
+    },
+    {
+      name: 'Qui suis-je ?',
+      href: '/About'
+    }
+  ]
 
   return (
     <nav className="navbar" data-testid="Navbar" style={styles}>
       <Logo />
+      <Navigation listNavItem={listNavItem} />
     </nav>
   )
 }
